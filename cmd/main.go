87bf21e15ee9788788a66c71082d067d112d896c
roney ruiz <roneyruizrojas@gmail.com>
2025-12-29 11:32:25 -0600

    feat: create a context playground

--- cmd/main.go
+++ cmd/main.go
@@ -1,43 +1,7 @@
 package main
 
-import (
-	"context"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
-)
-
-func execute(ctx context.Context) error {
-	for index := 1; index <= 10; index++ {
-		select {
-		case <-ctx.Done():
-			time.Sleep(500 * time.Millisecond)
-			return ctx.Err()
-		default:
-			time.Sleep(2 * time.Second)
-			println("Processing item", index)
-		}
-	}
-	return nil
-}
+import "vext/source/playground"
 
 func main() {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	signals := make(chan os.Signal, 1)
-	signal.Notify(signals, os.Interrupt, syscall.SIGTERM)
-
-	go func() {
-		<-signals
-		cancel()
-	}()
-
-	if err := execute(ctx); err != nil {
-		println("Execution interrupted:", err.Error())
-		os.Exit(1)
-	}
-
-	println("Execution completed successfully")
+	playground.RunContext()
 }
---
+++ source/playground/context.go
@@ -0,0 +1,42 @@
+package playground
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func execute(ctx context.Context) error {
+	for index := 1; index <= 10; index++ {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		default:
+			println("Processing item", index)
+			time.Sleep(1 * time.Second)
+		}
+	}
+	return nil
+}
+
+func RunContext() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		<-signals
+		cancel()
+	}()
+
+	if err := execute(ctx); err != nil {
+		println("Execution interrupted:", err.Error())
+		os.Exit(1)
+	}
+
+	println("Execution completed successfully")
+}
